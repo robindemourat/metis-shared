9119fe828c25b88133f9af13b831b86963095d94
Robin de Mourat <robin.demourat@gmail.com>
2018-01-17 18:19:27 +0100

    improve standalone cover

--- dist/components/views/static/StandaloneCover.js
+++ dist/components/views/static/StandaloneCover.js
@@ -15,24 +15,30 @@ var _Cover2 = _interopRequireDefault(_Cover);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 exports.default = function (_ref) {
-  var _ref$montage$data = _ref.montage.data,
+  var _ref$montage = _ref.montage,
+      title = _ref$montage.metadata.title,
+      _ref$montage$data = _ref$montage.data,
       coverColor = _ref$montage$data.cover_color,
-      title = _ref$montage$data.title,
       css = _ref$montage$data.css,
       _ref$renderingMode = _ref.renderingMode,
       renderingMode = _ref$renderingMode === undefined ? 'epub-fixed' : _ref$renderingMode,
       _ref$styles = _ref.styles,
       styles = _ref$styles === undefined ? '' : _ref$styles;
   return _react2.default.createElement(
-    'div',
+    'html',
     { className: 'StandaloneCover' },
-    _react2.default.createElement(_Cover2.default, { title: title, background: coverColor }),
+    _react2.default.createElement('head', null),
     _react2.default.createElement(
-      'style',
+      'body',
       null,
-      styles,
-      css.shared_css_code,
-      css[renderingMode + '_css_code']
+      _react2.default.createElement(_Cover2.default, { title: title, background: coverColor }),
+      _react2.default.createElement(
+        'style',
+        null,
+        styles,
+        css.shared_css_code,
+        css[renderingMode + '_css_code']
+      )
     )
   );
 };
--- src/components/views/static/StandaloneCover.js
+++ src/components/views/static/StandaloneCover.js
@@ -4,21 +4,26 @@ import Cover from './Cover';
 
 export default ({
   montage: {
+    metadata: {
+      title,
+    },
     data: {
       cover_color: coverColor,
-      title,
       css
     }
   },
   renderingMode = 'epub-fixed',
   styles = '',
 }) => (
-  <div className="StandaloneCover">
-    <Cover title={title} background={coverColor} />
-    <style>
-      {styles}
-      {css.shared_css_code}
-      {css[`${renderingMode}_css_code`]}
-    </style>
-  </div>
+  <html className="StandaloneCover">
+    <head></head>
+    <body>
+      <Cover title={title} background={coverColor} />
+      <style>
+        {styles}
+        {css.shared_css_code}
+        {css[`${renderingMode}_css_code`]}
+      </style>
+    </body>
+  </html>
 );
