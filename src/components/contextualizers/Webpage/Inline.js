56e2bc4289a3bf48db6682a2615c25243d3e0f4e
Robin de Mourat <robin.demourat@gmail.com>
2018-01-29 07:05:46 +0100

    improve inline webpage contextualizer

--- src/components/contextualizers/Webpage/Inline.js
+++ src/components/contextualizers/Webpage/Inline.js
@@ -6,22 +6,25 @@ const Inline = ({
   // contextualization,
   renderingMode
 }) => {
-  const content = contextualizer.content || '*';
+  let content;
   const url = resource.data.url;
 
   switch (renderingMode) {
     // interactive -> hyperlink
     case 'web':
     case 'epub-fixed':
+      content = contextualizer.content || '*';
       return (<a target="blank" href={url}>{content}</a>);
 
     // not interactive -> write link in parenthesis
     case 'micro':
+      content = contextualizer.content || '';
       return (<span>{content} ({url})</span>);
 
     // mix -> hyperlink + written link
     case 'epub-reflowable':
     case 'pdf':
+      content = contextualizer.content || '';
       return (<a target="blank" href={url}>{content} ({url})</a>);
     default:
       return null;
